fdaa0e8e4dcf876f534685c826229f0e6924e216
ikuma-t <tadokorodev@gmail.com>
2021-12-11 23:02:58 +0900

    JSONのキーにシンボルを使用するように変更
    - 元のJSONファイルと体系を合わせるため
    - 条件指定検索時にシンボルの方が扱いやすいため

--- lib/jp_local_gov/local_gov.rb
+++ lib/jp_local_gov/local_gov.rb
@@ -5,13 +5,13 @@ module JpLocalGov
     attr_reader :code, :prefecture_code, :prefecture, :prefecture_kana, :city, :city_kana, :prefecture_capital
 
     def initialize(data)
-      @code = data["code"]
-      @prefecture_code = data["prefecture_code"]
-      @prefecture = data["prefecture"]
-      @prefecture_kana = data["prefecture_kana"]
-      @city = data["city"]
-      @city_kana = data["city_kana"]
-      @prefecture_capital = data["prefecture_capital"]
+      @code = data[:code]
+      @prefecture_code = data[:prefecture_code]
+      @prefecture = data[:prefecture]
+      @prefecture_kana = data[:prefecture_kana]
+      @city = data[:city]
+      @city_kana = data[:city_kana]
+      @prefecture_capital = data[:prefecture_capital]
     end
   end
 end
