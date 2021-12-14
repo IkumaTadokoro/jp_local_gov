839704d4ac446ca255280217bf8030b97b0d8414
ikuma-t <tadokorodev@gmail.com>
2021-12-14 17:07:08 +0900

    JpLocalGov::Baseに対するテストファイルを作成

---
+++ spec/base_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/BlockLength
+
+RSpec.describe JpLocalGov::Base do
+  describe "#jp_local_gov" do
+    describe "argument :column_name" do
+      context "when the column_name specified" do
+        let(:model_class) do
+          klass = Class.new(ActiveRecord::Base) do
+            self.table_name = :insurances
+            include JpLocalGov
+            jp_local_gov :local_gov_code
+          end
+          klass.new(local_gov_code: "131016")
+        end
+
+        it "be able to convert local_gov_code" do
+          expect(model_class.local_government.city).to eq "千代田区"
+        end
+      end
+    end
+
+    describe "return value" do
+      let(:klass) do
+        Class.new(ActiveRecord::Base) do
+          self.table_name = :insurances
+          include JpLocalGov
+          jp_local_gov :local_gov_code
+        end
+      end
+
+      context "when the code exists" do
+        let(:model_class) { klass.new(local_gov_code: "131016") }
+        it { expect(model_class).to respond_to(:local_government) }
+        it { expect(model_class.local_government).to be_an_instance_of(JpLocalGov::LocalGov) }
+        it { expect(model_class.local_government.city).to eq "千代田区" }
+      end
+
+      context "when the code DOES NOT exist" do
+        let(:model_class) { klass.new(local_gov_code: "131017") }
+        it { expect(model_class).to respond_to(:local_government) }
+        it { expect(model_class.local_government).to be_nil }
+      end
+    end
+  end
+end
+
+# rubocop:enable Metrics/BlockLength
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require "jp_local_gov"
+require "jp_local_gov/base"
+require "active_record"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -11,5 +13,29 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  config.before(:suite) do
+    setup_db
+  end
+
+  config.after(:suite) do
+    teardown_db
+  end
+end
+
+def setup_db
+  ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
+
+  ActiveRecord::Schema.define(version: 1) do
+    create_table :insurances do |t|
+      t.string :local_gov_code
+    end
+  end
+end
+
+def teardown_db
+  ActiveRecord::Base.connection.tables.each do |table|
+    ActiveRecord::Base.connection.drop_table(table)
   end
 end
